386e1997f91286bdf119e3099babe01f0204149e
dezy433 <75358865+dezy433@users.noreply.github.com>
2022-04-20 00:31:02 +0100

    to test the html files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+
+def home():
+    
+   
+    return render_template('tools.html')
+
+
+
+
+if __name__ == "__main__":
+
+    app.run(debug = True)
