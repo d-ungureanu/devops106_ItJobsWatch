7211f4d67fca9db7935aa0eded48f818e635a0a0
em <emile_thompson@live.co.uk>
2022-04-20 15:05:45 +0100

    added functionalities for searches

--- src/itjobswatch_html_readers/perm_rates_job_search.py
+++ src/itjobswatch_html_readers/perm_rates_job_search.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 from src.http_management.http_manager import HttpManager
-
+from config_manager import itjobswatch_contract_search_test_page
 import re
+
+
+
 
 class PermRatesJobSearch:
 
@@ -14,7 +17,6 @@ class PermRatesJobSearch:
         page_check = self.job_search.find("td", {'class', re.compile("navContainer")}).find('h1', id='h1')
         if page_check is None:
             raise ValueError('Your role type led to a search try using something more specific')
-
 
 
     # Parse perm rates table
@@ -37,27 +39,44 @@ class PermRatesJobSearch:
 
         return captured_data
 
-    def get_median_salary(self):
-        return self.summary_table_data_selector("UK median annual salary")
+    def get_rank(self):
+        return self.summary_table_data_selector("Rank")
+
+    def get_rank_change_year_on_year(self):
+        return self.summary_table_data_selector("Rank change year-on-year")
+
+    def get_percentage_of_jobs_in_uk(self):
+        return self.summary_table_data_selector("As % of all permanent jobs advertised in the UK")
+
+    # def get_permanent_jobs_citing_name(self, name):
+    #     return self.summary_table_data_selector(f"Permanent jobs citing {name}")
+
+    # def get_median_annual_salary(self):
+    #     return self.summary_table_data_selector("Median annual salary (50 Percentile)")
+
+    def get_number_of_salaries_quoted(self):
+        return self.summary_table_data_selector("Number of salaries quoted")
 
     def get_median_salary_excluding_london(self):
         return self.summary_table_data_selector("UK excluding London median annual salary")
 
-    def get_salary_90th_percentile(self):
-        return self.summary_table_data_selector("90th Percentile")
-
-    def get_salary_10th_percentile(self):
-        return self.summary_table_data_selector("10th Percentile")
+    # def get_salary_90th_percentile(self):
+    #     return self.summary_table_data_selector("90th Percentile")
+    #
+    # def get_salary_10th_percentile(self):
+    #     return self.summary_table_data_selector("10th Percentile")
 
     def get_permanent_salary_data(self):
         return {"headers": self.get_summary_headers(),
-                "median_annual_salary": self.get_median_salary(),
-                "median_annual_salary_london_exc": self.get_median_salary_excluding_london(),
-                "10th_percentile": self.get_salary_10th_percentile(),
-                "90th_percentile": self.get_salary_90th_percentile()}
+                "Rank": self.get_rank(),
+                "Rank change year on year": self.get_rank_change_year_on_year(),
+                "median_annual_salary_london_exc": self.get_median_salary_excluding_london()}
+                # "10th_percentile": self.get_salary_10th_percentile(),
+                # "90th_percentile": self.get_salary_90th_percentile()
+
 
 if __name__ == '__main__':
+    print(PermRatesJobSearch("https://www.itjobswatch.co.uk/jobs/uk/sdet.do").summary_table())
 
-    print(PermRatesJobSearch("https://www.itjobswatch.co.uk/jobs/uk/sdet.do").get_summary_headers())
-
+    print(PermRatesJobSearch("https://www.itjobswatch.co.uk/jobs/uk/sdet.do").get_permanent_salary_data())
 
